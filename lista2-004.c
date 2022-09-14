c6d5f8748d89c4ed0593c3452b0d480f4946abdc
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-14 18:37:04 -0300

    Create lista2-004.c

---
+++ lista2-004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    x=150;
+    y=110;
+    z=0;
+    for(z; x >=y ; z++ ){
+        x += 2;
+        y += 3;
+    };
+    printf("o pedro vai passar josé em altura em %d anos", z);
+    
+    return 0;
+}
