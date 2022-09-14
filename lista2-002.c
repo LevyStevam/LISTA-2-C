ba95f89a260c76ea0a1a9edad818e74b98ff44d7
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-14 16:18:45 -0300

    Update lista2-002.c

--- lista2-002.c
+++ lista2-002.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 
-int main()
-{
-    int x;
-    for(x=0; x<8; x++){
-        puts("digite um número de 1-7:");
-        scanf("%d", &x);
+int main(){
+    char x;
+    do{
+        puts("digite um número 1-7: ");
+        scanf("%c", &x);
         switch(x){
-            case 1:
-            puts("domingo");
-            break;
-            case 2:
-            puts("segunda-feira");
-            break;
-            case 3:
-            puts("terça-feira");
-            break;
-            case 4:
-            puts("quarta-feira");
-            break;
-            case 5:
-            puts("quinta-feira");
-            break;
-            case 6:
-            puts("sexta-feira");
-            break;
-            case 7:
-            puts("sábado");
-            break;
+            case '0':
+                puts("terminando.");
+                break;
+            case '1':
+                puts("domingo");
+                break;
+            case '2':
+                puts("segunda-feira");
+                break;
+            case '3':
+                puts("terça-feira");
+                break;
+            case '4':
+                puts("quarta-feira");
+                break;
+            case '5':
+                puts("quinta-feira");
+                break;
+            case '6':
+                puts("sexta-feira");
+                break;
+            case '7':
+                puts("sábado");
+                break;
         }
         
-    }
+    }while(x != '0');   
+        
+    return 0;        
+    
 }
