aac93e5b7f414f4528983f9afed9a7dab003cb90
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-15 17:28:34 -0300

    Create lista2-010.c

---
+++ lista2-010.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x,i,y;
+    for(i=4;i<=22;i++){
+        x = i*4;
+        y = x*x;
+        printf("o %d multiplo de 4 tem valor %d e o seu quadrado é: %d \n",i,x,y);
+    };
+    
+}
