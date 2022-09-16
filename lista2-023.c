54e2cbe63d30fac42cd36591542ff995f3d03033
Levy Stevam Queiroz da Silva <stevamlevy@gmail.com>
2022-09-16 14:57:04 -0300

    Create lista2-023.c

---
+++ lista2-023.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int x=5;
+    puts("diferença entre pré incrementar e pos incrementar, exemplo número '5'.");
+    printf("número 5 com pós incremento: %d \n", x--);
+    printf("número 5 com pré incremento: %d \n", --x);
+    return 0;
+}
